8a5777d3552c3526fbe6137a4e792f3186873c8f
ASHISH KUMAR <42812707+ashishch164@users.noreply.github.com>
2021-07-02 12:26:49 +0530

    Add files via upload

---
+++ public/script.js
@@ -0,0 +1,42 @@
+let socket = io()
+$('#loginbox').show()
+$('#chatbox').hide()
+
+$('#btnstart').click(() => {
+    socket.emit('login', {
+        username: $('#inpusername').val(),
+        password: $('#inppassword').val()
+    })
+})
+socket.on('logged_in', () => {
+    $('#loginbox').hide()
+    window.alert('Sucessfully logged in')
+    $('#chatbox').show()
+
+})
+socket.on('login_failed', () => {
+    window.alert('wrong username or password')
+})
+
+$('#btnsendmsg').click(() => {
+    socket.emit('msg_send', {
+        to: $('#inptouser').val(),
+        msg: $('#inpnewmsg').val()
+
+    })
+})
+socket.on('msg_rcvd', (data) => {
+    $('#ulmsgs').append($('<li>').text(
+        `${data.from} :   ${data.msg}`
+    ))
+    console.log(data.from)
+})
+function myFunction() {
+    var x = document.getElementById("inppassword");
+    if (x.type === "password") {
+        x.type = "text";
+    } else {
+        x.type = "password";
+    }
+}
+
